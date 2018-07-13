cb25f21b0e38ee6ccadd1d6d37b072daa9c52757
Suyeon Jin <gmlakd8944@cnu.ac.kr>
2018-07-13 22:37:31 +0900

    add google map api

--- UPSA/app/src/main/java/com/project/bottomup/upsa/MainActivity.java
+++ UPSA/app/src/main/java/com/project/bottomup/upsa/MainActivity.java
@@ -1,13 +1,20 @@
 package com.project.bottomup.upsa;
 
+import android.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.MapFragment;
+
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        //지도 불러오기
+        FragmentManager fragmentManager = getFragmentManager();
+        MapFragment mapFragment = (MapFragment)fragmentManager.findFragmentById(R.id.map);
+        }
 }
