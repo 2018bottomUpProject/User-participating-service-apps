6038321a83b5c6cd9ecc2a147c326cdaf38902e2
Suyeon Jin <gmlakd8944@cnu.ac.kr>
2018-07-21 11:58:22 +0900

    modify service(background) first setting

--- UPSA/app/src/main/java/com/project/bottomup/upsa/BackgroundService.java
+++ UPSA/app/src/main/java/com/project/bottomup/upsa/BackgroundService.java
@@ -5,26 +5,31 @@ import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
 
-public class BackgroundService extends Service {
+public class BackgroundService extends Service{
     public BackgroundService() {
     }
 
+    //최초 생성되었을 때 한 번 실행하는 메서드
     @Override
     public void onCreate() {
         super.onCreate();
-        Log.d("service","onCreate()");
+        Log.i("service","onCreate()");
     }
 
+    //백그라운드로 실행되는 동작 메서드
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.d("service","onStartCommand()");
-        return super.onStartCommand(intent, flags, startId);
+        Log.i("service","onStartCommand()");
+
+        //메모리 공간 부족으로 서비스가 종료되었을 때, START_STICKY를 통해 재생성과 onStartCommand()호출
+        return START_STICKY;
     }
 
+    //서비스가 종료될 때 실행되는 메서드
     @Override
     public void onDestroy() {
         super.onDestroy();
-        Log.d("service","onDestroy");
+        Log.i("service","onDestroy()");
     }
 
     @Override
@@ -33,4 +38,5 @@ public class BackgroundService extends Service {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
     }
+
 }
