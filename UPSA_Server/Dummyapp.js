037e2917dd7a5aa2202bb0fc2cd523a32e471bf1
Jung Jinwook <jinwook1240@gmail.com>
2018-08-08 14:00:38 +0900

    Add f#3 (#27)
    * DB info updated
    
    updated Database Information Doc.
    
    Fixed https://github.com/2018bottomUpProject/User-participating-service-apps/issues/9
    
    * DB 초안
    
    DB 초안 및 필요한 함수들을 대강 구현함.
    
    https://github.com/2018bottomUpProject/User-participating-service-apps/issues/9
    
    * DB 관련 수정중
    
    DBConnector 수정
    
    Edit https://github.com/2018bottomUpProject/User-participating-service-apps/issues/9
    
    * 7.22 임시저장
    
    DB 관련 코드 짜던 중이었음.
    
    수정중 https://github.com/2018bottomUpProject/User-participating-service-apps/issues/9
    
    * .gitignore 수정
    
    개발 환경 관련 폴더 및 파일이 깃에 올라가지 않도록 수정함.
    
    * gitignore edit
    
    * Dummy Server Init
    
    더미 서버 관련 파일 만듬.
    ^
    
    init https://github.com/2018bottomUpProject/User-participating-service-apps/issues/3
    
    * Dummyapp Directory & Files init
    
    *I can't use Hangul on Webstorm console!*
    Initialized Dummy API Server's File and Directory structure.
    
    Editing https://github.com/2018bottomUpProject/User-participating-service-apps/issues/3
    
    * Dummy API Server App Completed & Router init
    
    set port to 8080
    DummyApp coding completed
    testing Document.js file(temporary save)
    
    Editing https://github.com/2018bottomUpProject/User-participating-service-apps/issues/3
    
    * using 'let' is needing strict mode
    
    to use 'let' keyword to initialize variable, we should use 'strict mode'
    so added "use strict"; code to top of code lines.
    
    Editing https://github.com/2018bottomUpProject/User-participating-service-apps/issues/3
    
    * title
    
    inoisdjoigsojdi
    
    Edit https://github.com/2018bottomUpProject/User-participating-service-apps/issues/3
    
    * hbs view engine setup
    
    view engine setup(express) finished
    
    Fixed https://github.com/2018bottomUpProject/User-participating-service-apps/issues/3
    
    * document, log, review dummy
    
    done making dummy server(document, log, review only)
    
    Editing https://github.com/2018bottomUpProject/User-participating-service-apps/issues/3
    
    * dummyserver editing
    
    alskgjdalks
    
    https://github.com/2018bottomUpProject/User-participating-service-apps/issues/3

---
+++ UPSA_Server/Dummyapp.js
@@ -0,0 +1,43 @@
+"use strict";
+let express = require("express");
+let app = express();
+let hbs = require("express-hbs");
+let path = require('path');
+
+let document_router = require("./Dummyapp/router/PLACE/Document");
+let locationfg_router = require("./Dummyapp/router/PLACE/LocationFG");
+let locationbg_router = require("./Dummyapp/router/PLACE/LocationBG");
+let log_router = require("./Dummyapp/router/PLACE/Log");
+let review_router = require("./Dummyapp/router/PLACE/Review");
+
+let permission_router = require("./Dummyapp/router/USER/Permission");
+let user_router = require("./Dummyapp/router/USER/User");
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use('/document', document_router);
+app.use('/locationfg', locationfg_router);
+app.use('/locationbg', locationbg_router);
+app.use('/log', log_router);
+app.use('/review', review_router);
+
+app.use('/permission', permission_router);
+app.use('/user', user_router);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+module.exports = app;
--- UPSA_Server/Main.js
+++ UPSA_Server/Main.js
@@ -1,8 +1,71 @@
-let DB = require("./DBConnector.js");
-process.on("SIGINT", function () {
+"use strict";
+let http = require('http');
+let https = require('https');
 
-    process.exit();
-});
-setInterval(function(){
-    console.log("asdf");
-},1000);
+
+//let DB = require("./DBConnector.js");
+let Dummyapp = require('./Dummyapp');
+let app;
+
+//if just want using Dummy, set this true.
+let startDummyServer = true;
+
+let port = 8080;
+let server;
+if(startDummyServer) {
+    /* Dummy HTTP server open                      *
+    *  if do not want, just set it with annotation*/
+
+    Dummyapp.set('port', port);
+    server = http.createServer(Dummyapp);
+    server.listen(Dummyapp.get('port'));
+    server.on('error', onError);
+    server.on('listening', onListening);
+}
+else{    
+    DB.init();
+    let sigint_func = function() {
+        DB.close();
+        process.exit();
+    };
+    process.on("SIGINT", sigint_func);
+
+
+}
+
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    let bind = typeof port === 'string'
+        ? 'Pipe ' + port
+        : 'Port ' + port;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+let debug = require('debug');
+function onListening() {
+    let addr = server.address();
+    let bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+    debug('Listening on ' + bind);
+}
