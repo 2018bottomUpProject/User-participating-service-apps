a518beb3432b10a32b03f71fc61c0ff630577859
Suyeon Jin <33437655+sy06@users.noreply.github.com>
2018-08-10 13:45:52 +0900

    Add f#20 (#31)
    * Add form to add menu when entering information
    
    * 식당/카페 등의  메뉴 정보를 등록할 수 있는 form 구현
    식당, 카페같은 경우 중요한 정보중 하나는 메뉴이므로 사용자들이 메뉴
    정보를 같이 등록하여 더 정확한 정보를 등록할 수 있도록 구현.
    * 메뉴(이름, 가격) dialogFragment에서 메뉴 정보 받아오는 callback 구현
    
    feat https://github.com/2018bottomUpProject/User-participating-service-apps/issues/20
    
    * Modify formation registration form
    
    * 해당 장소(시설)에 대한 문서를 작성할 때 보기 좋을 수 있도록 부가정보
    입력폼을 좀 더 크게 늘림.
    
    refactor
    https://github.com/2018bottomUpProject/User-participating-service-apps/issues/20
    
    * Modify menu print form
    
    해당 장소에 대한 메뉴를 출력할 때, 첫 번째 등록한 메뉴만 화면에 출력.
    그 다음 메뉴는 exception발생
    
    fix https://github.com/2018bottomUpProject/User-participating-service-apps/issues/20

--- UPSA/app/src/main/java/com/project/bottomup/upsa/AddActivity.java
+++ UPSA/app/src/main/java/com/project/bottomup/upsa/AddActivity.java
@@ -2,7 +2,6 @@ package com.project.bottomup.upsa;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
