7fffa354b6a005a7460e772ec53d612164ef62c6
leeeeeelee <yeyettt@cnu.ac.kr>
2018-08-15 16:45:12 +0900

    Change intro
    인트로화면을 gif로 바꿈.
    
    refactor https://github.com/2018bottomUpProject/User-participating-service-apps/issues/2

--- UPSA/app/src/main/java/com/project/bottomup/upsa/IntroActivity.java
+++ UPSA/app/src/main/java/com/project/bottomup/upsa/IntroActivity.java
@@ -4,6 +4,10 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
 
 public class IntroActivity extends Activity{
     @Override
@@ -11,6 +15,12 @@ public class IntroActivity extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.intro_layout); //xml , java 소스 연결
         MyThread.init(); //thread 시작
+
+        //setContentView(R.layout.activity_main);
+        ImageView iv=(ImageView)findViewById(R.id.iv);
+        GlideDrawableImageViewTarget imageViewTarget = new GlideDrawableImageViewTarget(iv);
+        Glide.with(this).load(R.raw.fireeee).into(imageViewTarget);
+
         Handler handler = new Handler();
         handler.postDelayed(new Runnable(){
             @Override
