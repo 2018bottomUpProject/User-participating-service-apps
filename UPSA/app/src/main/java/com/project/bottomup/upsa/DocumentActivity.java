c8b6a086ad5f99dafa0f590b675c8f0e2620b2c7
heesooooooooo <jhslight@cnu.ac.kr>
2018-10-31 20:38:03 +0900

    modified conflict error
    충돌 오류 해결
    
    core https://github.com/2018bottomUpProject/User-participating-service-apps/issues/65

--- UPSA/app/src/main/java/com/project/bottomup/upsa/DocumentActivity.java
+++ UPSA/app/src/main/java/com/project/bottomup/upsa/DocumentActivity.java
@@ -31,6 +31,17 @@ import org.json.JSONObject;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import java.io.BufferedReader;
 import java.io.IOException;
